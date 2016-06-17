f3575213ac583f64911995ec2fb324bd4d27de64
James Smith <jamessmith@MacBook-Pro.local>
2016-06-16 21:28:53 -0700

    Added code docs -jsdocs which is mucho importante

--- eventable.js
+++ eventable.js
@@ -12,28 +12,70 @@
         window.Eventable = factory();
     }
 })(function() {
+    /**
+     *
+     * @description Event manager
+     * @constructor Eventable
+     * @memberof! <global>
+     *
+     */
     var Eventable = function() {
         return this.init.apply( this, arguments );
     }
 
     Eventable.prototype = {
         constructor: Eventable,
+        
+        /**
+         *
+         * @description Eventable constructor method
+         * @memberof Eventable
+         * @method Eventable.init
+         *
+         */
         init: function() {
-            /*
-            * Unique ID for each handler.
-            */
+            /**
+             *
+             * @description Eventable unique ID
+             * @memberof Eventable
+             * @member _uid
+             * @private
+             *
+             */
             this._uid = 0;
-
-            /*
-            * Object to hold handler.
-            */
+            
+            /**
+             *
+             * @description Eventable event handlers object
+             * @memberof Eventable
+             * @member _handlers
+             * @private
+             *
+             */
             this._handlers = {};
         },
-
+        
+        /**
+         *
+         * @description Get a unique ID
+         * @memberof Controller
+         * @method getUID
+         * @returns number
+         *
+         */
         _getUID: function() {
             return this.uid += 1;
         },
-        
+
+        /**
+         *
+         * @description Eventable remove event handler
+         * @memberof Eventable
+         * @method off
+         * @param {string} event the event to remove handler for
+         * @param {function} handler the handler to remove
+         *
+         */
         off: function(event, handler) {
             if (!this._handlers[event]) {
                 return this;
@@ -48,6 +90,15 @@
             return this;
         },
 
+        /**
+         *
+         * @description Eventable add event handler
+         * @memberof Eventable
+         * @method on
+         * @param {string} event the event to listen for
+         * @param {function} handler the handler to call
+         *
+         */
         on: function(event, handler) {
             var events = event.split(' ');
 
@@ -66,6 +117,14 @@
             return this;
         },
 
+        /**
+         *
+         * @description Eventable fire an event
+         * @memberof Eventable
+         * @method fire
+         * @param {string} event the event to fire
+         *
+         */
         fire: function(event) {
             if (!this._handlers[event]) {
                 return this;
@@ -80,6 +139,16 @@
             return this;
         },
         
+        /**
+         *
+         * @description Eventable internal off method assumes event AND handler are good
+         * @memberof Eventable
+         * @method _off
+         * @param {string} event the event to remove handler for
+         * @param {function} handler the handler to remove
+         * @private
+         *
+         */
         _off: function(event, handler) {
             for (var i = 0; i < this._handlers[event].length; i++) {
                 if (handler._eventId === this._handlers[event][i]._eventId) {
@@ -90,6 +159,15 @@
             }
         },
         
+        /**
+         *
+         * @description Eventable completely remove all handlers and an event type
+         * @memberof Eventable
+         * @method _offed
+         * @param {string} event the event to remove handler for
+         * @private
+         *
+         */
         _offAll: function(event) {
             for (var i = this._handlers[event].length; i--;) {
                 this._handlers[event][i] = null;
